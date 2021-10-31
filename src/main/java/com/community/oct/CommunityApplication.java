03a5a9516b6652402b241e71a2855068840ee9d9
liyang1206 <1371773752@qq.com>
2021-10-31 23:11:22 +0800

    init repo

---
+++ src/main/java/com/community/oct/CommunityApplication.java
@@ -0,0 +1,10 @@
+package com.community.oct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class CommunityApplication {
+    public static void main(String args[]){SpringApplication.run(CommunityApplication.class,args);}
+}
---
+++ src/test/java/com/community/oct/OctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.oct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
